dcc5404ea5de48eeeccb8976d5ec50c1a497f566
9seconds <nineseconds@yandex.ru>
2022-11-18 11:40:55 +0300

    Add support for timeout flag

--- cli.go
+++ cli.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
+	"time"
+	"unicode"
 
 	"github.com/9seconds/chore/internal/env"
 	"github.com/alecthomas/kong"
@@ -34,6 +37,47 @@ func (c *CliNamespace) UnmarshalText(b []byte) error {
 	}
 
 	c.Value = text
+
+	return nil
+}
+
+type CliTimeout struct {
+	Value time.Duration
+}
+
+func (c *CliTimeout) UnmarshalText(b []byte) error {
+	number := true
+	text := string(b)
+
+	for _, char := range text {
+		if !unicode.IsDigit(char) {
+			number = false
+
+			break
+		}
+	}
+
+	if number {
+		value, err := strconv.ParseUint(text, 0, 64)
+		if err != nil {
+			return fmt.Errorf("incorrect count of seconds: %w", err)
+		}
+
+		c.Value = time.Duration(value) * time.Second
+
+		return nil
+	}
+
+	value, err := time.ParseDuration(text)
+	if err != nil {
+		return fmt.Errorf("incorrect duration: %w", err)
+	}
+
+	if value < 0 {
+		return fmt.Errorf("duration %s should be >=0", text)
+	}
+
+	c.Value = value
 
 	return nil
 }
--- cmd_run.go
+++ cmd_run.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/9seconds/chore/internal/argparse"
 	"github.com/9seconds/chore/internal/commands"
@@ -11,12 +13,20 @@ import (
 )
 
 type CliCmdRun struct {
+	Timeout CliTimeout `short:"t" placeholder:"TIMEOUT" help:"Timeout for a command execution."`
+
 	Namespace CliNamespace `arg:"" help:"Script namespace."`
 	Script    string       `arg:"" help:"Script name."`
 	Args      []string     `arg:"" optional:"" help:"Script arguments to use."`
 }
 
 func (c *CliCmdRun) Run(ctx Context) error {
+	var cancel context.CancelFunc
+
+	ctx.Context, cancel = c.prepare(ctx)
+
+	defer cancel()
+
 	executable, err := script.New(c.Namespace.Value, c.Script)
 	if err != nil {
 		return fmt.Errorf("cannot initialize script: %w", err)
@@ -66,4 +76,16 @@ func (c *CliCmdRun) Run(ctx Context) error {
 	return commands.ExitError{
 		Result: result,
 	}
+}
+
+func (c *CliCmdRun) prepare(ctx context.Context) (context.Context, context.CancelFunc) {
+	if c.Timeout.Value == 0 {
+		return context.WithCancel(ctx)
+	}
+
+	deadline := time.Now().Add(c.Timeout.Value)
+
+	log.Printf("command will cancel at %v", deadline)
+
+	return context.WithDeadline(ctx, deadline)
 }
