dd81b8d8a6dbbfa62d0e230fcd950f0ba8e9a66e
9seconds <nineseconds@yandex.ru>
2023-01-11 19:43:18 +0300

    Use @ as negate symbol

--- internal/argparse/argparse_test.go
+++ internal/argparse/argparse_test.go
@@ -18,7 +18,7 @@ func (suite *ParseTestSuite) TestInvalidArgument() {
 }
 
 func (suite *ParseTestSuite) TestUnexpectedFlag() {
-	for _, testName := range []string{"+c", "!c"} {
+	for _, testName := range []string{"+c", "@c"} {
 		testName := testName
 
 		suite.T().Run(testName, func(t *testing.T) {
@@ -31,7 +31,7 @@ func (suite *ParseTestSuite) TestUnexpectedFlag() {
 }
 
 func (suite *ParseTestSuite) TestIncorrectFlag() {
-	for _, testValue := range []string{"!", "+"} {
+	for _, testValue := range []string{"@", "+"} {
 		testValue := testValue
 
 		suite.T().Run(testValue, func(t *testing.T) {
@@ -58,15 +58,15 @@ func (suite *ParseTestSuite) TestMixed() {
 		"c=1",
 		"+x",
 		"+k",
-		"!k",
-		"!k",
-		"!j",
+		"@k",
+		"@k",
+		"@j",
 		"k=2",
 		"c=3",
 		"p=:v:d:e:",
 		"arg1",
 		"arg2",
-		":!j",
+		":@j",
 		":k=v",
 	}, argparse.DefaultListDelimiter)
 
@@ -81,7 +81,7 @@ func (suite *ParseTestSuite) TestMixed() {
 		"k": argparse.FlagFalse,
 		"j": argparse.FlagFalse,
 	}, parsed.Flags)
-	suite.Equal([]string{"arg1", "arg2", "!j", "k=v"}, parsed.Positional)
+	suite.Equal([]string{"arg1", "arg2", "@j", "k=v"}, parsed.Positional)
 }
 
 func (suite *ParseTestSuite) TestExplicitPositionals() {
